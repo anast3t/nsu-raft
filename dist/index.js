6c8b3dc1909bbdb8dab43868ca9038f946bc9278
anast3t <bortnikov20000@mail.ru>
2023-12-18 11:12:23 +0700

    dist remove

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const initRaftNode_1 = __importDefault(require("./initRaftNode"));
-const initExpress_1 = __importDefault(require("./initExpress"));
-(0, initRaftNode_1.default)();
-(0, initExpress_1.default)();
